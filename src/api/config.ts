8756924d642f19091d6ce45781e2e83a4712fafa
AbelCh <abelchocca1010@gmail.com>
2025-07-15 18:51:27 -0500

    Commit api

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://pizzeria-backend.up.railway.app',
+    baseURL: 'https://pizzeria-backend.up.railway.app/',
     timeout: 5000,
     withCredentials: true,
     headers: { 'Content-Type': 'application/json' }
